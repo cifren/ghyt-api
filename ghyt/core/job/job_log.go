738e967a458013559e33eb3348e0405746a37ac5
Francis <cifren@msn.com>
2020-02-26 12:43:24 +0100

    Add date to logs

--- ghyt/core/job/job_log.go
+++ ghyt/core/job/job_log.go
@@ -1,6 +1,11 @@
 package job
 
+import (
+  "time"
+)
+
 type JobLog struct {
+  CreatedAt time.Time `json:"created_at"`
   Request map[string]interface{} `json:"request"`
   Feedback []JobFeedback `json:"feedback"`
   ErrorMessage string `json:"error_message"`
